9a2e0f4d051af82895b380010777748d8e9dd62d
Scott Obernier <scottobernier@gmail.com>
2021-09-02 20:20:31 -0400

    java

---
+++ unittests/unitTests/src/Higher.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Higher {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = -25;
+        int c = higher(a,b);
+        System.out.println("\nThe higher of 25 and -25 is " + c);
+    }
+
+    public static int higher(int x, int y) {
+        if(x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    @Test 
+    public void testHigher(String arg) {
+        assertEquals(25, higher(25,25));
+        assertEquals(5, higher(3, 5));
+        assertEquals(6, higher(3, 5));
+    }
+}
